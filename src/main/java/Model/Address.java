ce8e4205ea18e4a98a5ac4308a0d1cb944cfa3b3
unknown <enzo-adrian2100@outlook.com>
2022-10-07 14:31:58 +0200

    Versión 0.0.15 Agregamos la clase Address

---
+++ src/main/java/Model/Address.java
@@ -0,0 +1,75 @@
+package Model;
+
+public class Address {
+
+	protected int id_Address;
+	protected City id_City;
+	protected String address_Name;
+	protected int address_Number;
+	protected int floor_Number;
+	protected int dept_Number;
+	
+	public int getId_Address() {
+		return id_Address;
+	}
+	public void setId_Address(int id_Address) {
+		this.id_Address = id_Address;
+	}
+	public City getId_City() {
+		return id_City;
+	}
+	public void setId_City(City id_City) {
+		this.id_City = id_City;
+	}
+	public String getAddress_Name() {
+		return address_Name;
+	}
+	public void setAddress_Name(String address_Name) {
+		this.address_Name = address_Name;
+	}
+	public int getAddress_Number() {
+		return address_Number;
+	}
+	public void setAddress_Number(int address_Number) {
+		this.address_Number = address_Number;
+	}
+	public int getFloor_Number() {
+		return floor_Number;
+	}
+	public void setFloor_Number(int floor_Number) {
+		this.floor_Number = floor_Number;
+	}
+	public int getDept_Number() {
+		return dept_Number;
+	}
+	public void setDept_Number(int dept_Number) {
+		this.dept_Number = dept_Number;
+	}
+	
+	public Address() {
+		super();
+	}
+	
+	public Address(int id_Address, City id_City, String address_Name, int address_Number, int floor_Number,
+			int dept_Number) {
+		super();
+		this.id_Address = id_Address;
+		this.id_City = id_City;
+		this.address_Name = address_Name;
+		this.address_Number = address_Number;
+		this.floor_Number = floor_Number;
+		this.dept_Number = dept_Number;
+	}
+	
+	public Address(City id_City, String address_Name, int address_Number, int floor_Number, int dept_Number) {
+		super();
+		this.id_City = id_City;
+		this.address_Name = address_Name;
+		this.address_Number = address_Number;
+		this.floor_Number = floor_Number;
+		this.dept_Number = dept_Number;
+	}
+	
+	
+	
+}
