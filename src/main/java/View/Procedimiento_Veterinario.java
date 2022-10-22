e4cffe41801131e571baca916e2e10ed842c1257
unknown <enzo-adrian2100@outlook.com>
2022-10-22 15:01:28 +0200

    Versión 0.0.38a Implementé la rel veterinario procedimiento

--- src/main/java/View/Procedimiento_Veterinario.java
+++ src/main/java/View/Procedimiento_Veterinario.java
@@ -1,35 +1,34 @@
 package View;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-
-import Control.Connect;
-import View.Usuario_Sucursal.ComboItem;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.awt.event.ActionEvent;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
-public class Procedimiento_Sucursal extends JFrame {
+import Control.Connect;
+import View.Veterinario_Sucursal.ComboItem;
+
+public class Procedimiento_Veterinario extends JFrame {
 
 	private JPanel contentPane;
-	private JComboBox cbProcedimiento;
-	private JComboBox cbSucursal;
 	private JTable table;
+	private JComboBox cbProcedimiento;
+	private JComboBox cbVeterinario;
 
 	class ComboItem
 	{
@@ -59,7 +58,8 @@ public class Procedimiento_Sucursal extends JFrame {
 	    }
 	}
 	
-	public DefaultComboBoxModel cargarSucursal() {
+
+	public DefaultComboBoxModel cargarVeterinario() {
 		Connection cn = null;
 		PreparedStatement pst = null;
 		ResultSet result = null;
@@ -69,16 +69,14 @@ public class Procedimiento_Sucursal extends JFrame {
 		
 		try {
 			cn = (Connection) Connect.getConexion();
-			String SSQL = "Select *\r\n"
-					+ "FROM Branch\r\n"
-					+ "INNER JOIN Address ON Branch.id_Address = Address.id_Address\r\n"
-					+ "ORDER BY Branch.id_Address";
+			String SSQL = "Select *"
+					+ "FROM Veterinarian";
 			pst = cn.prepareStatement(SSQL);
 			result = pst.executeQuery();
 			modelo.addElement(new ComboItem("",""));
 			
 			while (result.next()) {
-				modelo.addElement(new ComboItem(result.getString("address_Name")+" - "+result.getString("address_Number"),result.getString("id_Branch")));
+				modelo.addElement(new ComboItem(result.getString("name")+" "+result.getString("surname"),result.getString("id_Veterinarian")));
 				
 			}
 			cn.close();
@@ -91,6 +89,7 @@ public class Procedimiento_Sucursal extends JFrame {
 		return modelo;
     }
 	
+
 	public DefaultComboBoxModel cargarProcedimiento() {
 		Connection cn = null;
 		PreparedStatement pst = null;
@@ -124,11 +123,12 @@ public class Procedimiento_Sucursal extends JFrame {
 		return modelo;
     }
 	
+
 	void mostrarTabla(){
         
         DefaultTableModel modelo = new DefaultTableModel();
         
-        modelo.setColumnIdentifiers(new Object[] {"Sucursal","Procedimiento"});
+        modelo.setColumnIdentifiers(new Object[] {"Veterinario","Procedimiento"});
        
         table.setModel(modelo);
         
@@ -138,12 +138,11 @@ public class Procedimiento_Sucursal extends JFrame {
        
         try {
         	Connection con = Connect.getConexion();
-        	PreparedStatement ps = con.prepareStatement("SELECT Address.address_Name, Address.address_Number, proced_Name,CONVERT(varchar(10),proced_Date,103) AS pd ,CONVERT(varchar(10),proced_Time,8) as pt\r\n"
-        			+ "FROM Rel_Branch_Medical_P\r\n"
-        			+ "INNER JOIN Medical_Procedure ON Medical_Procedure.id_Procedure = Rel_Branch_Medical_P.id_Procedure\r\n"
+        	PreparedStatement ps = con.prepareStatement("SELECT name, surname, proced_Name,CONVERT(varchar(10),proced_Date,103) AS pd ,CONVERT(varchar(10),proced_Time,8) as pt\r\n"
+        			+ "FROM Rel_Veterinarian_Medical_P\r\n"
+        			+ "INNER JOIN Medical_Procedure ON Medical_Procedure.id_Procedure = Rel_Veterinarian_Medical_P.id_Procedure\r\n"
         			+ "INNER JOIN Procedure_Type ON Procedure_Type.id_Procedure_Type = Medical_Procedure.id_Procedure_Type\r\n"
-        			+ "INNER JOIN Branch ON Branch.id_Branch = Rel_Branch_Medical_P.id_Branch\r\n"
-        			+ "INNER JOIN Address ON Branch.id_Address = Address.id_Address;" );
+        			+ "INNER JOIN Veterinarian ON Veterinarian.id_Veterinarian = Rel_Veterinarian_Medical_P.id_Veterinarian;" );
             ResultSet rs = ps.executeQuery();
             while (rs.next()){
                 datos[0] = rs.getString(1)+" "+rs.getString(2);
@@ -171,7 +170,7 @@ public class Procedimiento_Sucursal extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Procedimiento_Sucursal frame = new Procedimiento_Sucursal();
+					Procedimiento_Veterinario frame = new Procedimiento_Veterinario();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -180,17 +179,17 @@ public class Procedimiento_Sucursal extends JFrame {
 		});
 	}
 
-	public int existeRel(Object procedimiento, Object sucursal) {
+	public int existeRel(Object procedimiento, Object veterinario) {
 		Connection cn = null;
 		PreparedStatement pst = null;
 		ResultSet result = null;
 		
 		try {
 			cn = (Connection) Connect.getConexion();
-			String SSQL = "SELECT count(*) FROM Rel_Branch_Medical_P WHERE id_Procedure = ? AND id_Branch = ?;";
+			String SSQL = "SELECT count(*) FROM Rel_Veterinarian_Medical_P WHERE id_Procedure = ? AND id_Veterinarian = ?;";
 			pst = cn.prepareStatement(SSQL);
 			pst.setString(1,(String) procedimiento);
-			pst.setString(2, (String) sucursal);
+			pst.setString(2, (String) veterinario);
 			result = pst.executeQuery();
 			
 			if (result.next()) {
@@ -212,13 +211,13 @@ public class Procedimiento_Sucursal extends JFrame {
 	
 	private void limpiar() {
 		cbProcedimiento.setSelectedIndex(0);
-		cbSucursal.setSelectedIndex(0);
+		cbVeterinario.setSelectedIndex(0);
 		
 	}
 	/**
 	 * Create the frame.
 	 */
-	public Procedimiento_Sucursal() {
+	public Procedimiento_Veterinario() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 750, 491);
 		contentPane = new JPanel();
@@ -227,8 +226,8 @@ public class Procedimiento_Sucursal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblTitulo = new JLabel("Asociar procedimiento a sucursal");
-		lblTitulo.setBounds(108, 11, 209, 14);
+		JLabel lblTitulo = new JLabel("Asociar veterinario a procedimiento");
+		lblTitulo.setBounds(108, 11, 224, 14);
 		contentPane.add(lblTitulo);
 		
 		JLabel lblProcedimiento = new JLabel("Procedimiento");
@@ -240,39 +239,39 @@ public class Procedimiento_Sucursal extends JFrame {
 		contentPane.add(cbProcedimiento);
 		cbProcedimiento.setModel(cargarProcedimiento());
 		
-		JLabel lblSucursal = new JLabel("Sucursal");
-		lblSucursal.setBounds(430, 107, 92, 14);
-		contentPane.add(lblSucursal);
+		JLabel lblVeterinario = new JLabel("Veterinario");
+		lblVeterinario.setBounds(430, 107, 92, 14);
+		contentPane.add(lblVeterinario);
 		
-		cbSucursal = new JComboBox();
-		cbSucursal.setBounds(532, 103, 192, 22);
-		contentPane.add(cbSucursal);
-		cbSucursal.setModel(cargarSucursal());
+		cbVeterinario = new JComboBox();
+		cbVeterinario.setBounds(532, 103, 192, 22);
+		contentPane.add(cbVeterinario);
+		cbVeterinario.setModel(cargarVeterinario());
 		
 		JButton btnAgregar = new JButton("Agregar");
 		btnAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Object proced = cbProcedimiento.getSelectedItem();
-				Object sucursal = cbSucursal.getSelectedItem();
+				Object veterinario = cbVeterinario.getSelectedItem();
 				
 				int result = 0;
 				
 				try {
 					Connection con = Connect.getConexion();
-					PreparedStatement ps = con.prepareStatement("INSERT INTO Rel_Branch_Medical_P (id_Branch,id_Procedure) VALUES (?,?)" );
+					PreparedStatement ps = con.prepareStatement("INSERT INTO Rel_Veterinarian_Medical_P (id_Veterinarian,id_Procedure) VALUES (?,?)" );
 					
 					
 					if (((ComboItem) proced).getValue() == "") {
 						JOptionPane.showMessageDialog(null, "Seleccione un procedimiento");
 					}else {
-						if(((ComboItem) sucursal).getValue() == ""){
-							JOptionPane.showMessageDialog(null, "Seleccione una sucursal");
+						if(((ComboItem) veterinario).getValue() == ""){
+							JOptionPane.showMessageDialog(null, "Seleccione un veterinario");
 						}else {
-							if(existeRel(((ComboItem) cbProcedimiento.getSelectedItem()).getValue(),((ComboItem) cbSucursal.getSelectedItem()).getValue())!=0) {
-								JOptionPane.showMessageDialog(null, "Procedimiento ya se encuentra en la sucursal");
+							if(existeRel(((ComboItem) cbProcedimiento.getSelectedItem()).getValue(),((ComboItem) cbVeterinario.getSelectedItem()).getValue())!=0) {
+								JOptionPane.showMessageDialog(null, "Veterinario ya está asociado al procedimiento");
 							}else {
 								ps.setString(2, ((ComboItem) proced).getValue());
-								ps.setString(1, ((ComboItem) sucursal).getValue());
+								ps.setString(1, ((ComboItem) veterinario).getValue());
 								
 							}
 						}
@@ -283,11 +282,11 @@ public class Procedimiento_Sucursal extends JFrame {
 					result = ps.executeUpdate();
 					
 					if(result > 0){
-		                JOptionPane.showMessageDialog(null, "Procedimiento colocado");
+		                JOptionPane.showMessageDialog(null, "Veterinario colocado");
 		                limpiar();
 		                mostrarTabla();
 		            } else {
-		                JOptionPane.showMessageDialog(null, "Error al colocar procedimiento");
+		                JOptionPane.showMessageDialog(null, "Error al colocar veterinario");
 		                limpiar();
 		            }
 				
@@ -307,23 +306,23 @@ public class Procedimiento_Sucursal extends JFrame {
 		btnEliminar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Object proced = cbProcedimiento.getSelectedItem();
-				Object sucursal = cbSucursal.getSelectedItem();
+				Object veterinario = cbVeterinario.getSelectedItem();
 				
 				int result = 0;
 				try {
 					Connection con = Connect.getConexion();
-					PreparedStatement ps = con.prepareStatement("DELETE FROM Rel_Branch_Medical_P WHERE id_Branch = ? AND id_Procedure = ?;" );
-					ps.setString(1, ((ComboItem) sucursal).getValue());
+					PreparedStatement ps = con.prepareStatement("DELETE FROM Rel_Veterinarian_Medical_P WHERE id_Veterinarian = ? AND id_Procedure = ?;" );
+					ps.setString(1, ((ComboItem) veterinario).getValue());
 					ps.setString(2, ((ComboItem) proced).getValue());
 					
 					result = ps.executeUpdate();
 					
 					if(result > 0){
-		                JOptionPane.showMessageDialog(null, "Procedimiento removido de la sucursal");
+		                JOptionPane.showMessageDialog(null, "Veterinario removido del procedimiento");
 		                limpiar();
 		                mostrarTabla();
 		            } else {
-		                JOptionPane.showMessageDialog(null, "Error al remover procedimiento");
+		                JOptionPane.showMessageDialog(null, "Error al remover veterinario");
 		                limpiar();
 		            }
 					
@@ -355,4 +354,5 @@ public class Procedimiento_Sucursal extends JFrame {
 		scrollPane.setViewportView(table);
 		mostrarTabla();
 	}
+
 }
