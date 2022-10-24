9257d104c64be84853902dafec0e5db4d05b039d
unknown <enzo-adrian2100@outlook.com>
2022-10-24 21:52:39 +0200

    Versión 0.0.41a Añadí una clase support

--- src/main/java/Support/Supporting_SS.java
+++ src/main/java/Support/Supporting_SS.java
@@ -1,8 +1,9 @@
 package Support;
 
-public class Supporting_SS {
+//Esta clase contiene funciones utilizadas para soporte. Ej: validación de datos, uso general, etc
+public class Supporting_SS {   
 
-	public static boolean validateNum(String num) {
+	public static boolean validateNum(String num) {     //Recibe una cadena y devuelve true si todos los caracteres son números
 		boolean flagError = false;
 		
 		for(int i=0; i < num.length(); i++ ) {
