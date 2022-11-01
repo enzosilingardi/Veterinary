adec81ca97444c7ca617a5158c3de113c0f52af3
unknown <enzo-adrian2100@outlook.com>
2022-11-01 14:42:57 +0100

    Versión 0.0.48a Añadí una tabla y el botón modificar al Tipo_Proveedor

--- src/main/java/View/Tipo_Proveedor.java
+++ src/main/java/View/Tipo_Proveedor.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import Control.Connect;
 import View.Instrumento.ComboItem;
@@ -23,13 +24,56 @@ import java.util.regex.Pattern;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class Tipo_Proveedor extends JFrame {
 
 	private JPanel contentPane;
 	private JComboBox cbTipo;
 	private JTextField txtNombre;
+	private JTable table;
 
+	
+
+	void mostrarTabla(){
+        
+        DefaultTableModel modelo = new DefaultTableModel();
+        
+        modelo.setColumnIdentifiers(new Object[] {"ID","Tipo de proveedor"});
+       
+        table.setModel(modelo);
+        
+        
+        
+        String datos[] = new String[2];
+       
+        try {
+        	Connection con = Connect.getConexion();
+        	PreparedStatement ps = con.prepareStatement("SELECT * FROM Provider_Type" );
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                datos[0] = rs.getString(1);
+                datos[1] = rs.getString(2);
+                
+                modelo.addRow(datos);
+
+            }
+            
+            table.setModel(modelo);
+
+            table.getColumnModel().getColumn(0).setMaxWidth(0);
+    		table.getColumnModel().getColumn(0).setMinWidth(0);
+    		table.getColumnModel().getColumn(0).setPreferredWidth(0);
+    		table.getColumnModel().getColumn(0).setResizable(false);
+        } catch(SQLException E) {
+			JOptionPane.showMessageDialog(null,E);
+		}catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+    }
 	/**
 	 * Launch the application.
 	 */
@@ -120,16 +164,12 @@ public class Tipo_Proveedor extends JFrame {
 	public Tipo_Proveedor() {
 		setTitle("Tipo Proveedor");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 281);
+		setBounds(100, 100, 581, 394);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblTitulo = new JLabel("Tipo de proveedor");
-		lblTitulo.setBounds(168, 11, 119, 14);
-		contentPane.add(lblTitulo);
 		
 		JButton btnAgregar = new JButton("Agregar");
 		btnAgregar.addActionListener(new ActionListener() {
@@ -159,6 +199,7 @@ public class Tipo_Proveedor extends JFrame {
 					if(result > 0){
 		                JOptionPane.showMessageDialog(null, "Tipo guardado");
 		                limpiar();
+		                mostrarTabla();
 		            } else {
 		                JOptionPane.showMessageDialog(null, "Error al guardar tipo");
 		                limpiar();
@@ -174,7 +215,7 @@ public class Tipo_Proveedor extends JFrame {
 				
 			}
 		});
-		btnAgregar.setBounds(55, 130, 89, 23);
+		btnAgregar.setBounds(352, 130, 89, 23);
 		contentPane.add(btnAgregar);
 		
 		JButton btnEliminar = new JButton("Eliminar");
@@ -182,38 +223,36 @@ public class Tipo_Proveedor extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				
 				int result = 0;
-				String nombre = txtNombre.getText();
-
+				int fila = table.getSelectedRow();
+				int id = Integer.parseInt(table.getValueAt(fila,0).toString());
 				
 				try {
 					Connection con = Connect.getConexion();
-					PreparedStatement ps = con.prepareStatement("DELETE FROM Provider_Type WHERE type_Name = ?;" );
-					if(tipoEnUso(nombre) != 0) {
-						JOptionPane.showMessageDialog(null, "Tipo está en uso, por favor elimine todos los registros relacionados");
-					}else {
-						ps.setString(1, nombre);
-					}
+					PreparedStatement ps = con.prepareStatement("DELETE FROM Provider_Type WHERE id_Provider_Type = ?" );
+					
+					ps.setInt(1, id);
+					
 					
 					result = ps.executeUpdate();
 					
 					if(result > 0){
 		                JOptionPane.showMessageDialog(null, "Tipo eliminado");
-		                limpiar();
+		               mostrarTabla();
 		            } else {
 		                JOptionPane.showMessageDialog(null, "Error al eliminar tipo");
-		                limpiar();
+		                
 		            }
-					
+					con.close();
 				}catch(SQLException E) {
 					E.printStackTrace();
+					JOptionPane.showMessageDialog(null, "Tipo está en uso, por favor elimine todos los registros relacionados");
 				}catch (ClassNotFoundException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
 			}
 		});
-		btnEliminar.setBounds(251, 130, 89, 23);
+		btnEliminar.setBounds(466, 130, 89, 23);
 		contentPane.add(btnEliminar);
 		
 		JButton btnVolver = new JButton("Volver");
@@ -222,17 +261,62 @@ public class Tipo_Proveedor extends JFrame {
 				dispose();
 			}
 		});
-		btnVolver.setBounds(304, 178, 89, 23);
+		btnVolver.setBounds(466, 321, 89, 23);
 		contentPane.add(btnVolver);
 		
 		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setBounds(55, 61, 46, 14);
+		lblNombre.setBounds(362, 61, 46, 14);
 		contentPane.add(lblNombre);
 		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(153, 58, 203, 20);
+		txtNombre.setBounds(418, 58, 137, 20);
 		contentPane.add(txtNombre);
 		txtNombre.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(25, 11, 266, 333);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int result = 0;
+				
+				int fila = table.getSelectedRow();
+				String tipo = table.getValueAt(fila,1).toString();
+				int id = Integer.parseInt(table.getValueAt(fila,0).toString());
+				try {
+					Connection con = Connect.getConexion();
+					PreparedStatement ps = con.prepareStatement("UPDATE Provider_Type SET type_Name = ? WHERE id_Provider_Type = ?" );  //Crea el statement
+					
+					ps.setString(1, tipo);
+					ps.setInt(2, id);
+					
+					
+					result = ps.executeUpdate();
+					
+					if(result > 0){
+		                JOptionPane.showMessageDialog(null, "Animal modificado");
+		                mostrarTabla();
+		            } else {
+		                JOptionPane.showMessageDialog(null, "Error al modificar animal");
+		                mostrarTabla();
+		            }
+					
+				}catch(SQLException E) {
+					E.printStackTrace();
+				}catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnModificar.setBounds(412, 180, 89, 23);
+		contentPane.add(btnModificar);
+		
+		mostrarTabla();
 	}
-
 }
