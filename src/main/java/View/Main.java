209b75052cb5a98c0011cf12f1d07c1778ac9192
unknown <jonasarome21@gmail.com>
2022-11-08 23:29:32 -0300

    Version 0.0.52a Intentamos colocarle una imagen de icono al main

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import java.awt.Image;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
@@ -17,11 +18,15 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Font;
+import javax.swing.border.LineBorder;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
-
+	private ImageIcon imagen;
+	private Icon icono;
 	
 	
 	public Icon setIcono(String url, JButton boton) {
@@ -53,22 +58,130 @@ public class Main extends JFrame {
 			}
 		});
 	}
+	
+	private void pintarImagen(JLabel lbl,String ruta){
+		this.imagen = new ImageIcon(ruta);
+		this.icono = new ImageIcon(this.imagen.getImage().getScaledInstance(
+				lbl.getWidth(), 
+				lbl.getHeight(), 
+				Image.SCALE_DEFAULT
+				)
+		);
+		this.repaint();
+	}
 
 	/**
 	 * Create the frame.
 	 */
-	public Main(final String perfil) {
+	public Main() {
 		setTitle("Principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 690, 555);
+		setBounds(100, 100, 800, 600);
+		
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 22, 180, 541);
+		panel.setBackground(new Color(64, 224, 208));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnInstrumentos = new JButton("Instrumentos");
+		btnInstrumentos.setBounds(0, 456, 180, 23);
+		panel.add(btnInstrumentos);
+		btnInstrumentos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Tabla_Instrumento ti = new Tabla_Instrumento(perfil);
+				ti.setVisible(true);
+			}
+		});
+		
+		JButton btnSucursales = new JButton("Sucursales");
+		btnSucursales.setBounds(0, 388, 180, 23);
+		panel.add(btnSucursales);
+		btnSucursales.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Tabla_Sucursales sucursales = new Tabla_Sucursales(perfil);
+				sucursales.setVisible(true);
+			}
+		});
+		
+		JButton btnVeterinarios = new JButton("Veterinarios");
+		btnVeterinarios.setBounds(0, 354, 180, 23);
+		panel.add(btnVeterinarios);
+		btnVeterinarios.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Tabla_Veterinario tv = new Tabla_Veterinario(perfil);
+				tv.setVisible(true);
+			}
+		});
+		
+		JButton btnMascotas = new JButton("Mascotas");
+		btnMascotas.setBounds(0, 286, 180, 23);
+		panel.add(btnMascotas);
+		
+		JButton btnQuirofanos = new JButton("Quirófanos");
+		btnQuirofanos.setBounds(0, 422, 180, 23);
+		panel.add(btnQuirofanos);
+		btnQuirofanos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Tabla_Quirofano tq = new Tabla_Quirofano(perfil);
+				tq.setVisible(true);
+			}
+		});
+		
+		JButton btnHistoriales = new JButton("Historiales");
+		btnHistoriales.setBounds(0, 320, 180, 23);
+		panel.add(btnHistoriales);
+		
+		JButton btnTurnos = new JButton("Turnos");
+		btnTurnos.setFont(new Font("Roboto", Font.PLAIN, 11));
+		btnTurnos.setForeground(new Color(0, 0, 0));
+		btnTurnos.setBackground(new Color(255, 255, 255));
+		btnTurnos.setBounds(0, 218, 180, 23);
+		panel.add(btnTurnos);
+		btnTurnos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Tabla_Turnos tt = new Tabla_Turnos(perfil);
+				tt.setVisible(true);
+			}
+		});
+		
+		JButton btnClientes = new JButton("Clientes");
+		btnClientes.setBounds(0, 252, 180, 23);
+		panel.add(btnClientes);
+		btnClientes.setBackground(new Color(240, 240, 240));
+		
+		JButton btnUsuarios = new JButton("Usuarios");
+		btnUsuarios.setBounds(0, 490, 180, 23);
+		panel.add(btnUsuarios);
+		btnUsuarios.setIcon(new ImageIcon(Main.class.getResource("/Imagenes/IconoUsuario.png")));
+		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblLogo.setBounds(10, 11, 160, 160);
+		panel.add(lblLogo);
+		
+		btnUsuarios.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Tabla_Usuarios usuarios = new Tabla_Usuarios(perfil);
+				usuarios.setVisible(true);
+			}
+		});
+		
 		
 		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
+		menuBar.setBounds(0, 0, 786, 22);
+		contentPane.add(menuBar);
 		
 		JMenu mnVentas = new JMenu("Ventas");
 		mnVentas.setHorizontalAlignment(SwingConstants.CENTER);
 		menuBar.add(mnVentas);
-		
 		
 		
 		JMenuItem mntmProductos = new JMenuItem("Productos");
@@ -113,114 +226,26 @@ public class Main extends JFrame {
 		JMenuItem mntmPresupuestos = new JMenuItem("Presupuestos");
 		mnVentas.add(mntmPresupuestos);
 		
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(0, 0, 128));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton btnClientes = new JButton("Clientes");
-		btnClientes.setBackground(new Color(240, 240, 240));
 		btnClientes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Tabla_Clientes tc = new Tabla_Clientes(perfil);
 				tc.setVisible(true);
 			}
 		});
-		btnClientes.setBounds(10, 11, 104, 81);
-		contentPane.add(btnClientes);
-		
-		JButton btnTurnos = new JButton("Turnos");
-		btnTurnos.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Tabla_Turnos tt = new Tabla_Turnos(perfil);
-				tt.setVisible(true);
-			}
-		});
-		btnTurnos.setBounds(124, 11, 104, 81);
-		contentPane.add(btnTurnos);
-		
-		JButton btnHistoriales = new JButton("Historiales");
 		btnHistoriales.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Tabla_Historial th = new Tabla_Historial(perfil);
 				th.setVisible(true);
 			}
 		});
-		btnHistoriales.setBounds(238, 11, 104, 81);
-		contentPane.add(btnHistoriales);
-		
-		JButton btnVeterinarios = new JButton("Veterinarios");
-		btnVeterinarios.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Tabla_Veterinario tv = new Tabla_Veterinario(perfil);
-				tv.setVisible(true);
-			}
-		});
-		btnVeterinarios.setBounds(352, 11, 104, 81);
-		contentPane.add(btnVeterinarios);
-		
-		JButton btnSucursales = new JButton("Sucursales");
-		btnSucursales.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Tabla_Sucursales sucursales = new Tabla_Sucursales(perfil);
-				sucursales.setVisible(true);
-			}
-		});
-		btnSucursales.setBounds(466, 11, 104, 81);
-		contentPane.add(btnSucursales);
-		
-		JButton btnUsuarios = new JButton("Usuarios");
-		btnUsuarios.setIcon(new ImageIcon(Main.class.getResource("/Imagenes/IconoUsuario.png")));
-		btnUsuarios.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Tabla_Usuarios usuarios = new Tabla_Usuarios(perfil);
-				usuarios.setVisible(true);
-			}
-		});
-		btnUsuarios.setBounds(461, 371, 205, 114);
-		contentPane.add(btnUsuarios);
-		
-		JButton btnQuirofanos = new JButton("Quirófanos");
-		btnQuirofanos.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Tabla_Quirofano tq = new Tabla_Quirofano(perfil);
-				tq.setVisible(true);
-			}
-		});
-		btnQuirofanos.setBounds(124, 103, 104, 81);
-		contentPane.add(btnQuirofanos);
-		
-		JButton btnMascotas = new JButton("Mascotas");
 		btnMascotas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Tabla_Mascota tm = new Tabla_Mascota(perfil);
 				tm.setVisible(true);
 			}
 		});
-		btnMascotas.setBounds(238, 103, 104, 81);
-		contentPane.add(btnMascotas);
 		
-		JButton btnDirecciones = new JButton("Direcciones");
-		btnDirecciones.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Tabla_Direccion td = new Tabla_Direccion(perfil);
-				td.setVisible(true);
-			}
-		});
-		btnDirecciones.setBounds(352, 103, 104, 81);
-		contentPane.add(btnDirecciones);
-		
-		JButton btnInstrumentos = new JButton("Instrumentos");
-		btnInstrumentos.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Tabla_Instrumento ti = new Tabla_Instrumento(perfil);
-				ti.setVisible(true);
-			}
-		});
-		btnInstrumentos.setBounds(466, 103, 104, 81);
-		contentPane.add(btnInstrumentos);
+		this.pintarImagen(lblLogo,"/images/VetCareSoftLogo.png");
 	}
 
 	public Main() {
