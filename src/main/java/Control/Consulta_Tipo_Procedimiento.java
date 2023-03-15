fb015cc069bb8409e0fe27aa23c2dbd4a36f194b
unknown <enzo-adrian2100@outlook.com>
2023-03-14 23:58:08 -0300

    Version 2.0.2a Comenzamos a colocar las consultas en la clase Control

--- src/main/java/Control/Consulta_Tipo_Procedimiento.java
+++ src/main/java/Control/Consulta_Tipo_Procedimiento.java
@@ -1,5 +1,190 @@
 package Control;
 
-public class Consulta_Tipo_Procedimiento {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import Model.ControlFiles;
+
+public class Consulta_Tipo_Procedimiento {
+	public static Connection tabla(DefaultTableModel modelo, JTable table) {
+		modelo.setColumnIdentifiers(new Object[] {"ID","Tipo de procedimiento"});       //Nombre de las columnas
+	       
+        table.setModel(modelo);        //Setea el modelo
+        
+        
+        
+        String datos[] = new String[2];         //Declara que va a haber 2 columnas
+       
+        try {
+        	Connection con = Connect.getConexion();       //Realiza la conexión
+        	//Sentencia sql
+        	PreparedStatement ps = con.prepareStatement("SELECT * FROM Procedure_Type" );
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){                     //Carga las columnas de la base de datos en la tabla
+                datos[0] = rs.getString(1);
+                datos[1] = rs.getString(2);
+                
+                modelo.addRow(datos);
+
+            }
+            
+            table.setModel(modelo);        //Setea el modelo
+
+            table.getColumnModel().getColumn(0).setMaxWidth(0);    // los 4 siguientes hacen que la columna del id sea invisible para el usuario
+    		table.getColumnModel().getColumn(0).setMinWidth(0);
+    		table.getColumnModel().getColumn(0).setPreferredWidth(0);
+    		table.getColumnModel().getColumn(0).setResizable(false);
+        } catch(SQLException E) {
+			JOptionPane.showMessageDialog(null,E);
+		}catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
+	public static int existe(String nombre) {
+		Connection cn = null;
+		PreparedStatement pst = null;
+		ResultSet result = null;
+		
+		try {
+			cn = (Connection) Connect.getConexion();   //Realiza la conexión
+			
+			String SSQL = "SELECT count(proced_Name) FROM Procedure_Type WHERE proced_Name = ?;";   //Sentencia Sql
+			pst = cn.prepareStatement(SSQL);
+			pst.setString(1, nombre);
+			result = pst.executeQuery();
+			
+			if (result.next()) {
+				return result.getInt(1);          //Si ya existe el tipo, la variable se pone en 1
+			}
+			return 1;
+			
+		} catch(SQLException e) {
+			JOptionPane.showMessageDialog(null,e);
+			return 1;
+		}catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public static Connection agregar(String nombre) {
+		int result = 0;
+		
+		try {
+			Connection con = Connect.getConexion();       //Realiza la conexión
+			
+			PreparedStatement ps = con.prepareStatement("INSERT INTO Procedure_Type (proced_Name) VALUES (?)" );
+			
+			if(existe(nombre) != 0) {          //Revisa si ya existe el tipo
+				
+				JOptionPane.showMessageDialog(null, "Tipo ya existe");
+			}else {
+				ps.setString(1, nombre);
+			}
+			
+			
+			result = ps.executeUpdate();
+			
+			if(result > 0){
+                JOptionPane.showMessageDialog(null, "Tipo guardado");      //Si fue exitoso, lo muestra mediante un mensaje en pantalla y lo añade al log
+                ControlFiles.addContent("Se ha añadido el tipo de procedimiento "+nombre);
+                
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al guardar Tipo");     //En caso de fallar, lo avisa en pantalla
+                
+            }
+		
+			
+		}catch(SQLException E) {
+			E.printStackTrace();
+		}catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
+	public static Connection eliminar(int id, String nombre) {
+		int result = 0;
+		
+		try {
+			Connection con = Connect.getConexion();     //Realiza la conexión
+			
+			PreparedStatement ps = con.prepareStatement("DELETE FROM Procedure_Type WHERE id_Procedure_Type = ?" );
+			
+			ps.setInt(1, id);
+			
+			
+			result = ps.executeUpdate();
+			
+			if(result > 0){
+                JOptionPane.showMessageDialog(null, "Tipo eliminado");        //Si fue exitoso, lo muestra mediante un mensaje en pantalla y lo añade al log
+                
+                ControlFiles.addContent("Se ha eliminado el tipo de procedimiento "+nombre);
+               
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al eliminar tipo");    //En caso de fallar, lo avisa en pantalla
+                
+            }
+			con.close();
+		}catch(SQLException E) {
+			E.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Tipo está en uso, por favor elimine todos los registros relacionados");      //En caso de fallar, lo avisa en pantalla
+		}catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	public static Connection modificar(String tipo, int id, String nombre) {
+		int result = 0;
+		
+
+		try {
+			Connection con = Connect.getConexion();     //Realiza la conexión
+			
+			PreparedStatement ps = con.prepareStatement("UPDATE Procedure_Type SET proced_Name = ? WHERE id_Procedure_Type = ?" );  //Crea el statement
+			
+			ps.setString(1, tipo);
+			ps.setInt(2, id);
+			
+			
+			result = ps.executeUpdate();
+			
+			if(result > 0){
+                JOptionPane.showMessageDialog(null, "Tipo modificado");   //Si fue exitoso, lo muestra mediante un mensaje en pantalla y lo añade al log
+                ControlFiles.addContent("Se ha modificado el tipo de procedimiento "+nombre);
+               
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al modificar tipo");    //En caso de fallar, lo avisa en pantalla
+                
+            }
+			
+		}catch(SQLException E) {
+			E.printStackTrace();
+		}catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
 }
--- src/main/java/View/Tipo_Procedimiento.java
+++ src/main/java/View/Tipo_Procedimiento.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import Control.Connect;
+import Control.Consulta_Tipo_Procedimiento;
 import Model.ControlFiles;
 
 import javax.swing.JLabel;
@@ -35,39 +36,8 @@ public class Tipo_Procedimiento extends JFrame {
         
         DefaultTableModel modelo = new DefaultTableModel(); 
         
-        modelo.setColumnIdentifiers(new Object[] {"ID","Tipo de procedimiento"});       //Nombre de las columnas
-       
-        table.setModel(modelo);        //Setea el modelo
+        Consulta_Tipo_Procedimiento.tabla(modelo, table);
         
-        
-        
-        String datos[] = new String[2];         //Declara que va a haber 2 columnas
-       
-        try {
-        	Connection con = Connect.getConexion();       //Realiza la conexión
-        	//Sentencia sql
-        	PreparedStatement ps = con.prepareStatement("SELECT * FROM Procedure_Type" );
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()){                     //Carga las columnas de la base de datos en la tabla
-                datos[0] = rs.getString(1);
-                datos[1] = rs.getString(2);
-                
-                modelo.addRow(datos);
-
-            }
-            
-            table.setModel(modelo);        //Setea el modelo
-
-            table.getColumnModel().getColumn(0).setMaxWidth(0);    // los 4 siguientes hacen que la columna del id sea invisible para el usuario
-    		table.getColumnModel().getColumn(0).setMinWidth(0);
-    		table.getColumnModel().getColumn(0).setPreferredWidth(0);
-    		table.getColumnModel().getColumn(0).setResizable(false);
-        } catch(SQLException E) {
-			JOptionPane.showMessageDialog(null,E);
-		}catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
         
     }
 	/**
@@ -86,36 +56,7 @@ public class Tipo_Procedimiento extends JFrame {
 		});
 	}
 
-	
-	public int existeTipo(String nombre) {        //Este procedimiento revisa si ya existe el tipo
-		Connection cn = null;
-		PreparedStatement pst = null;
-		ResultSet result = null;
-		
-		try {
-			cn = (Connection) Connect.getConexion();   //Realiza la conexión
-			
-			String SSQL = "SELECT count(proced_Name) FROM Procedure_Type WHERE proced_Name = ?;";   //Sentencia Sql
-			pst = cn.prepareStatement(SSQL);
-			pst.setString(1, nombre);
-			result = pst.executeQuery();
-			
-			if (result.next()) {
-				return result.getInt(1);          //Si ya existe el tipo, la variable se pone en 1
-			}
-			return 1;
-			
-		} catch(SQLException e) {
-			JOptionPane.showMessageDialog(null,e);
-			return 1;
-		}catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		return 0;
-		
-		
-	}
+
 	
 	
 	
@@ -154,39 +95,9 @@ public class Tipo_Procedimiento extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				String nombre = txtNombre.getText();
 				
-				int result = 0;
+				Consulta_Tipo_Procedimiento.agregar(nombre);
 				
-				try {
-					Connection con = Connect.getConexion();       //Realiza la conexión
-					
-					PreparedStatement ps = con.prepareStatement("INSERT INTO Procedure_Type (proced_Name) VALUES (?)" );
-					
-					if(existeTipo(nombre) != 0) {          //Revisa si ya existe el tipo
-						
-						JOptionPane.showMessageDialog(null, "Tipo ya existe");
-					}else {
-						ps.setString(1, nombre);
-					}
-					
-					
-					result = ps.executeUpdate();
-					
-					if(result > 0){
-		                JOptionPane.showMessageDialog(null, "Tipo guardado");      //Si fue exitoso, lo muestra mediante un mensaje en pantalla y lo añade al log
-		                ControlFiles.addContent("Se ha añadido el tipo de procedimiento "+nombre);
-		                limpiar();
-		            } else {
-		                JOptionPane.showMessageDialog(null, "Error al guardar Tipo");     //En caso de fallar, lo avisa en pantalla
-		                limpiar();
-		            }
-				
-					
-				}catch(SQLException E) {
-					E.printStackTrace();
-				}catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				mostrarTabla();
 			}
 		});
 		btnAgregar.setBounds(367, 146, 89, 23);
@@ -196,38 +107,13 @@ public class Tipo_Procedimiento extends JFrame {
 		btnEliminar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				int result = 0;
 				int fila = table.getSelectedRow();
 				int id = Integer.parseInt(table.getValueAt(fila,0).toString());
+				String nombre = table.getValueAt(fila,1).toString();
 				
-				try {
-					Connection con = Connect.getConexion();     //Realiza la conexión
-					
-					PreparedStatement ps = con.prepareStatement("DELETE FROM Procedure_Type WHERE id_Procedure_Type = ?" );
-					
-					ps.setInt(1, id);
-					
-					
-					result = ps.executeUpdate();
-					
-					if(result > 0){
-		                JOptionPane.showMessageDialog(null, "Tipo eliminado");        //Si fue exitoso, lo muestra mediante un mensaje en pantalla y lo añade al log
-		                
-		                ControlFiles.addContent("Se ha eliminado el tipo de procedimiento "+table.getValueAt(fila,1).toString());
-		               mostrarTabla();
-		            } else {
-		                JOptionPane.showMessageDialog(null, "Error al eliminar tipo");    //En caso de fallar, lo avisa en pantalla
-		                
-		            }
-					con.close();
-				}catch(SQLException E) {
-					E.printStackTrace();
-					JOptionPane.showMessageDialog(null, "Tipo está en uso, por favor elimine todos los registros relacionados");      //En caso de fallar, lo avisa en pantalla
-				}catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				Consulta_Tipo_Procedimiento.eliminar(id, nombre);
 				
+				mostrarTabla();
 			}
 		});
 		btnEliminar.setBounds(466, 146, 89, 23);
@@ -247,37 +133,16 @@ public class Tipo_Procedimiento extends JFrame {
 		JButton btnModificar = new JButton("Modificar");          //Este botón permite modificar el tipo selecconado
 		btnModificar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int result = 0;
 				
 				int fila = table.getSelectedRow();
 				String tipo = table.getValueAt(fila,1).toString();
 				int id = Integer.parseInt(table.getValueAt(fila,0).toString());
-				try {
-					Connection con = Connect.getConexion();     //Realiza la conexión
-					
-					PreparedStatement ps = con.prepareStatement("UPDATE Procedure_Type SET proced_Name = ? WHERE id_Procedure_Type = ?" );  //Crea el statement
-					
-					ps.setString(1, tipo);
-					ps.setInt(2, id);
-					
-					
-					result = ps.executeUpdate();
-					
-					if(result > 0){
-		                JOptionPane.showMessageDialog(null, "Tipo modificado");   //Si fue exitoso, lo muestra mediante un mensaje en pantalla y lo añade al log
-		                ControlFiles.addContent("Se ha modificado el tipo de procedimiento "+table.getValueAt(fila,1).toString());
-		                mostrarTabla();
-		            } else {
-		                JOptionPane.showMessageDialog(null, "Error al modificar tipo");    //En caso de fallar, lo avisa en pantalla
-		                mostrarTabla();
-		            }
-					
-				}catch(SQLException E) {
-					E.printStackTrace();
-				}catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				String nombre = table.getValueAt(fila,1).toString();
+				
+				Consulta_Tipo_Procedimiento.modificar(tipo, id, nombre);
+				
+				mostrarTabla();
+				
 			}
 		});
 		btnModificar.setBounds(413, 180, 89, 23);
